4c25dffc088bc11ac843983efe089953af96fd83
Aster Ginete <asterginete@yahoo.com>
2023-08-29 21:34:19 +0900

    add mongodb integration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 import productRoutes from './routes/productRoutes';
+import connectDB from './config/db';
 
 const app = express();
 const PORT = 3000;
+
+connectDB();
 
 app.use(express.json());
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,9 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { User } from '../models/user';
+import User from '../models/userSchema';
 import { generateToken, hashPassword, comparePassword } from '../auth/authService';
 
 const router = Router();
-
-let users: User[] = [];
 
 // Register
 router.post('/register', [
@@ -18,15 +16,30 @@ router.post('/register', [
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const hashedPassword = await hashPassword(req.body.password);
-  const user: User = {
-    id: Date.now(),
-    ...req.body,
-    password: hashedPassword,
-    role: 'user'
-  };
-  users.push(user);
-  res.status(201).json({ message: 'User registered successfully' });
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
 
 // Login
@@ -39,42 +52,102 @@ router.post('/login', [
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const user = users.find(u => u.email === req.body.email);
-  if (user && await comparePassword(req.body.password, user.password)) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
     const token = generateToken(user.id, user.role);
     res.json({ token });
-  } else {
-    res.status(401).send('Invalid credentials');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
   }
 });
 
-// Read all users
-router.get('/', (req: Request, res: Response) => {
-  res.json(users);
+// Get all users (this might be an admin-only route in a real-world application)
+router.get('/', async (req: Request, res: Response) => {
+  try {
+    const users = await User.find().select('-password'); // Exclude password from the result
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
 
-// Read one user by ID
-router.get('/:id', (req: Request, res: Response) => {
-  const user = users.find(u => u.id === +req.params.id);
-  if (user) res.json(user);
-  else res.status(404).send('User not found');
+// Get user by ID
+router.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password');
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
 
 // Update user by ID
-router.put('/:id', (req: Request, res: Response) => {
-  const index = users.findIndex(u => u.id === +req.params.id);
-  if (index !== -1) {
-    users[index] = { id: +req.params.id, ...req.body };
-    res.json(users[index]);
-  } else {
-    res.status(404).send('User not found');
+router.put('/:id', [
+  body('name').optional().notEmpty().withMessage('Name is required'),
+  body('email').optional().isEmail().withMessage('Valid email is required'),
+  body('password').optional().isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+], async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    if (name) user.name = name;
+    if (email) user.email = email;
+    if (password) {
+      const hashedPassword = await hashPassword(password);
+      user.password = hashedPassword;
+    }
+
+    await user.save();
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
   }
 });
 
 // Delete user by ID
-router.delete('/:id', (req: Request, res: Response) => {
-  users = users.filter(u => u.id !== +req.params.id);
-  res.status(204).send();
+router.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    await User.deleteOne({ _id: req.params.id });
+
+    res.status(204).send(); // 204 No Content response for successful delete
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
 
 export default router;
